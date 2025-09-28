d3be5dcdb86ed5432ab572b65ebe358a269ad4f8
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-09-28 23:41:23 +0600

    Previous changes

--- KeepNotes/main/admin.py
+++ KeepNotes/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Notes, UserInfos, SharedNotes 
 
 # Register your models here.
+admin.site.register(Notes)
+admin.site.register(UserInfos)
+admin.site.register(SharedNotes)
